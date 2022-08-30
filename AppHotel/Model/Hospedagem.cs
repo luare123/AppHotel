308707a42b0ae1c04031fc41dcc3a1b4d77d5efe
Usuario <Usuario@CPD03-M15>
2022-08-30 14:04:41 -0300

    Adicionar arquivos de projeto.

---
+++ AppHotel/Model/Hospedagem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace AppHotel.Model
+{
+    public class Hospedagem
+    {
+         public CategoriaQuarto Quarto { get; set; }
+
+    }
+}
---
+++ AppHotel/Model/Suite.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppHotel.Model
+{
+    internal class Suite
+    {
+        public string descricao { get; set; }
+        public double ValorDiariaAdulto { get; set; }
+        public double ValorDiariaCrianca { get; set; }
+    }
+}
